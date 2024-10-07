03ffd370f294fc65246a6243c3deaff6bcd55730
Daniel Judd <ZMAC322@live.rhul.ac.uk>
2024-10-07 13:33:20 +0100

    Added boilerplate code for Grade.java

---
+++ CW1/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
@@ -0,0 +1,18 @@
+package uk.ac.rhul.cs2800.model;
+
+public class Grade {
+
+    private int score;
+
+    public Grade(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
