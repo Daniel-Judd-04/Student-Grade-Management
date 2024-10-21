c8456db8517a5aab33e6ed1a2195e80f8165ee5f
Daniel Judd <ZMAC322@live.rhul.ac.uk>
2024-10-21 14:15:10 +0100

    Added a @BeforeEach to reduce repeated code in StudentTest

--- CW1/src/test/java/uk/ac/rhul/cs2800/model/StudentTest.java
+++ CW1/src/test/java/uk/ac/rhul/cs2800/model/StudentTest.java
@@ -3,40 +3,59 @@ package uk.ac.rhul.cs2800.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.rhul.cs2800.exceptions.NoGradeAvailableException;
 import uk.ac.rhul.cs2800.exceptions.NoRegistrationException;
 
 public class StudentTest {
 
+  private Student student;
+  private Module module1;
+  private Module module2;
+  private Grade grade1;
+  private Grade grade2;
+
+  @BeforeEach
+  public void setup() {
+    student = new Student();
+    module1 = new Module("CS2800", "Software Engineering", true);
+    module2 = new Module("CS2855", "Databases", true);
+    grade1 = new Grade(module1, 5);
+    grade2 = new Grade(module2, 3);
+  }
+
+//  Qs:
+//    // Is this allowed?
+//    // Can I refactor my tests to make them more concise and efficient (e.g. combine to gettersAndSettersTest())
+  // Does computeAverageTest need to throw NoRegistrationException?
+  // Will I be penalised for back tracking commits or too many commits? Do I need to use Issues?
+//  }
+
   @Test
   public void computeAverageTest() throws NoGradeAvailableException {
     // Test 1
-    Student student = new Student();
-    student.addGrade(new Grade(5));
-    student.addGrade(new Grade(3));
+    student.addGrade(grade1);
+    student.addGrade(grade2);
     assertEquals(4, student.computeAverage());
   }
 
   @Test
   public void registerModuleTest() {
     // Test 3
-    Student student = new Student();
-    Module module = new Module();
-    student.registerModule(module);
+    student.registerModule(module1);
     assertEquals(1, student.getRegistrations().size());
-    assertEquals(module, student.getRegistration(0).getModule());
+    assertEquals(module1, student.getRegistration(0).getModule());
   }
 
   @Test
   public void computeAverageNoGradeTest() {
     // Test 6
-    Student student = new Student();
     assertThrows(NoGradeAvailableException.class, student::computeAverage);
   }
 
   @Test
-  public void constructStudentFieldsTest() {
+  public void constructFieldsTest() {
     // Test 9
     long id = 1234L;
     String firstName = "John";
@@ -56,34 +75,26 @@ public class StudentTest {
   @Test
   public void getGradeTest() throws NoGradeAvailableException, NoRegistrationException {
     // Test 10
-    Module module = new Module();
-    Grade grade = new Grade(module);
-    Student student = new Student();
-    student.registerModule(module);
-    student.addGrade(grade);
+    student.registerModule(module1);
+    student.addGrade(grade1);
 
-    assertEquals(grade, student.getGrade(module));
+    assertEquals(grade1, student.getGrade(module1));
   }
 
   @Test
   public void getGradeNoGradeTest() {
     // Test 12
-    Module module = new Module();
-    Student student = new Student();
-    student.registerModule(module);
+    student.registerModule(module1);
 
-    assertThrows(NoGradeAvailableException.class, () -> student.getGrade(module));
+    assertThrows(NoGradeAvailableException.class, () -> student.getGrade(module1));
   }
 
   @Test
   public void getGradeNoRegistrationTest() {
     // Test 13
-    Module module = new Module();
-    Grade grade = new Grade(module);
-    Student student = new Student();
-    student.addGrade(grade);
+    student.addGrade(grade1);
 
-    assertThrows(NoRegistrationException.class, () -> student.getGrade(module));
+    assertThrows(NoRegistrationException.class, () -> student.getGrade(module1));
 
   }
 
@@ -91,14 +102,6 @@ public class StudentTest {
   public void getGradeWithMultipleGradesAndModulesTest()
       throws NoRegistrationException, NoGradeAvailableException {
     // Test 14
-    Module module1 = new Module("CS2800", "Software Engineering", true);
-    Module module2 = new Module("CS2855", "Databases", true);
-
-    Grade grade1 = new Grade(module1);
-    Grade grade2 = new Grade(module2);
-
-    Student student = new Student();
-
     student.registerModule(module1);
     student.registerModule(module2);
 
@@ -107,5 +110,13 @@ public class StudentTest {
 
     assertEquals(grade1, student.getGrade(module1));
     assertEquals(grade2, student.getGrade(module2));
+  }
+
+  @Test
+  public void setFieldsTest() {
+    // Test 15
+    student.setId(1234);
+    assertEquals(1234, student.getId());
+    // ... (implement other setters and getter tests)
   }
 }
